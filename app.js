46af12a4c03c241a3a2657e7d0f8b751e8015c7e
Caleb Lugo <cnl1211999@gmail.com>
2023-09-27 16:05:00 -0700

    Add operator function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Basic arithmetic functions
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +13,20 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+// Returns the appropriate operation from the user input
+function operate(operator, num1, num2) {
+    switch(operate) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Illegal input!";
+    }
 }
